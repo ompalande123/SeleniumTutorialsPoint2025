9d66bf5126b91c8646712a618424a26de4c15699
Omkar Palande <ompalande123@gmail.com>
2025-11-01 22:15:38 +0530

    updated pom file

--- src/test/java/samplePrograms/TestPrograms.java
+++ src/test/java/samplePrograms/TestPrograms.java
@@ -8,131 +8,88 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 public class TestPrograms {
-
-	@Test
-	public void swap2NumbersWithoutTemp() {
-		int a = 51;
-		int b = 101;
-
-		a = a + b; // a=152
-		b = a - b; // 152-101 ==51
-		a = a - b; // 152-51 == 101
-		System.out.println("After swap;; a=" + a + " and b=" + b);
-
-	}
-
-	@Test
-	public void swap2numbersUsingTempVar() {
-		int a = 20;
-		int b = 40;
-
-		int temp;
-		temp = a;
-		a = b;
-		b = temp;
-
-		System.out.println("After swap;; a=" + a + " and b=" + b);
-	}
-
-	@Test
-	public void fibonacciSEries() {
-		int num1 = 0;
-		int num2 = 1;
-
-		int limit = 10;
-
-		System.out.println(num1);
-		System.out.println(num2);
-		for (int i = 0; i < limit; i++) {
-			int nextnum = num1 + num2;
-			num1 = num2;
-			num2 = nextnum;
-			System.out.println(nextnum);
-		}
-
-	}
-
-	@Test
-	public void reverseAString() {
-		String str = "Hello Tester";
-		StringBuilder sb = new StringBuilder(str);
-		sb.reverse();
-		System.out.println(sb.toString());
-	}
-
-	@Test
-	public void reverseOnlyWord() {
-		String input = "Today is Friday";
-		String expected = "Friday is Today";
-
-		String[] newStr = input.split(" ");
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = newStr.length - 1; i >= 0; i--) {
-			sb.append(newStr[i]);
-			if (i != 0) {
-				sb.append(" ");
-			}
-		}
-
-		System.out.println("Expected String is: " + sb.toString());
-
-	}
-
-	@Test
-	public void testArrayList() {
-
-		List<String> strList = new ArrayList<String>();
-		strList.add("One");
-		strList.add("Two");
-		strList.add("Three");
-		strList.add("Four");
-		strList.add("Five");
-
-		System.out.println(strList.get(4)); // ArrayList preserves indexing
-
-		// Add element at specific position
-		strList.add(0, "Tester");
-		System.out.println(strList);
-
-		strList.addFirst("FirstPos");
-//		strList.addLast("LastPos");
-		System.out.println(strList);
-
-		System.out.println(strList.contains("Two")); // true
-
-		strList.set(0, "UpdatedString"); // To update at specific position
-		System.out.println(strList);
-		
-		//Traverse using for each loop
-		System.out.println("Using For Each::");
-		for(String str:strList) {
-			System.out.println(str);
-		}
-		
-		//Traverse using ITerator
-		Iterator itr=strList.iterator();
-		System.out.println("Using Iterator::");
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
-			
-		}
-		
-		
-
-	}
-	
-	
-	
-	@Test
-	public void testLinkedArrayList() {
-		
-		List<String> linkedArrayList=new LinkedList<String>();
-		linkedArrayList.add("One");
-		
-		
-	}
-	
-	
-
+	/*
+	 * @Test public void swap2NumbersWithoutTemp() { int a = 51; int b = 101;
+	 * 
+	 * a = a + b; // a=152 b = a - b; // 152-101 ==51 a = a - b; // 152-51 == 101
+	 * System.out.println("After swap;; a=" + a + " and b=" + b);
+	 * 
+	 * }
+	 * 
+	 * @Test public void swap2numbersUsingTempVar() { int a = 20; int b = 40;
+	 * 
+	 * int temp; temp = a; a = b; b = temp;
+	 * 
+	 * System.out.println("After swap;; a=" + a + " and b=" + b); }
+	 * 
+	 * @Test public void fibonacciSEries() { int num1 = 0; int num2 = 1;
+	 * 
+	 * int limit = 10;
+	 * 
+	 * System.out.println(num1); System.out.println(num2); for (int i = 0; i <
+	 * limit; i++) { int nextnum = num1 + num2; num1 = num2; num2 = nextnum;
+	 * System.out.println(nextnum); }
+	 * 
+	 * }
+	 * 
+	 * @Test public void reverseAString() { String str = "Hello Tester";
+	 * StringBuilder sb = new StringBuilder(str); sb.reverse();
+	 * System.out.println(sb.toString()); }
+	 * 
+	 * @Test public void reverseOnlyWord() { String input = "Today is Friday";
+	 * String expected = "Friday is Today";
+	 * 
+	 * String[] newStr = input.split(" "); StringBuilder sb = new StringBuilder();
+	 * 
+	 * for (int i = newStr.length - 1; i >= 0; i--) { sb.append(newStr[i]); if (i !=
+	 * 0) { sb.append(" "); } }
+	 * 
+	 * System.out.println("Expected String is: " + sb.toString());
+	 * 
+	 * }
+	 * 
+	 * @Test public void testArrayList() {
+	 * 
+	 * List<String> strList = new ArrayList<String>(); strList.add("One");
+	 * strList.add("Two"); strList.add("Three"); strList.add("Four");
+	 * strList.add("Five");
+	 * 
+	 * System.out.println(strList.get(4)); // ArrayList preserves indexing
+	 * 
+	 * // Add element at specific position strList.add(0, "Tester");
+	 * System.out.println(strList);
+	 * 
+	 * strList.addFirst("FirstPos"); // strList.addLast("LastPos");
+	 * System.out.println(strList);
+	 * 
+	 * System.out.println(strList.contains("Two")); // true
+	 * 
+	 * strList.set(0, "UpdatedString"); // To update at specific position
+	 * System.out.println(strList);
+	 * 
+	 * //Traverse using for each loop System.out.println("Using For Each::");
+	 * for(String str:strList) { System.out.println(str); }
+	 * 
+	 * //Traverse using ITerator Iterator itr=strList.iterator();
+	 * System.out.println("Using Iterator::"); while(itr.hasNext()) {
+	 * System.out.println(itr.next());
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * @Test public void testLinkedArrayList() {
+	 * 
+	 * List<String> linkedArrayList=new LinkedList<String>();
+	 * linkedArrayList.add("One");
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 */
 }
